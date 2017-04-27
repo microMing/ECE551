f0f2c32e47818ac76ebd36b4989ebe745de3fa04
LZhangWisc <siriuslzhang@gmail.com>
2017-04-26 23:04:29 -0500

    Add files via upload

--- ex12/test_related_files/uart_tx.sv
+++ ex12/test_related_files/uart_tx.sv
@@ -1,15 +1,16 @@
 //Author: Liang Zhang
 //Email: lzhang432@wisc.edu
-module uart_tx(clk, rst_n, TX, trmt, tx_data, tx_done);
+module uart_tx(clk, rst_n, tx, strt_tx, tx_data, tx_done);
 //clk, rst_n;
 input clk, rst_n;
 //output
-output TX;
+output tx;
 //data in interface
-input trmt;
+input strt_tx;
 input [7:0] tx_data;
 output tx_done;
 
+wire trmt = strt_tx;
 //50Mhz clock, Baud rate: 19200. each data is 2604 clock cycles
 //UART: Start (1'b0), data(LSB->MSB), stop(1'b1)
 
@@ -29,7 +30,7 @@ localparam transmitting = 1'b1;
 //declare
 wire shift;
 wire load;
-wire TX;
+wire tx;
 reg tx_done;
 reg start_reg;
 
@@ -60,7 +61,7 @@ begin
   end
 end
 
-assign TX = shift_reg[0];
+assign tx = shift_reg[0];
 
 //baud_counter
 always@(posedge clk, negedge rst_n) begin
